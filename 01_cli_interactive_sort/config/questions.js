61aed161ca069fc6372f497158dfc77ece7009c0
Taras Mikulskyi <tarasmikulskyi@ukr.net>
2023-10-21 15:27:44 +0300

    Created questions config

---
+++ 01_cli_interactive_sort/config/questions.js
@@ -0,0 +1,4 @@
+export const questionsConfig = {
+    sentenceQuestion: "Hello. Enter 10 words or digits dividing them in spaces: ",
+    sortingQuestion: "How would you like to sort values: \n 1. Words by name (from A to Z).\n 2. Show digits from the smallest.\n 3. Show digits from the biggest.\n 4. Words by quantity of letters.\n 5. Only unique words.\n 6. Only unique words and numbers.\n\n Select (1-6) and press ENTER: ",
+}
