bc291b8f359e8451e4a771cd4d485565b39d828e
Taras Mikulskyi <tarasmikulskyi@ukr.net>
2023-10-30 13:14:41 +0200

    Created bot config

---
+++ 05_telegram_bot_exchange_rates/src/config/bot.js
@@ -0,0 +1,13 @@
+export const botConfig = {
+    botToken: '6780389342:AAGrN4vJW37lIvVIIX0NOEIgovVQ9plk59Q',
+    reply_markup: {
+        resize_keyboard: true,
+        one_time_keyboard: true,
+        keyboard: [
+            [
+                "USD",
+                "EUR",
+            ],
+        ],
+    },
+}
