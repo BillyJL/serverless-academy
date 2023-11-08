010b2142f4e121b5ce45c48ac577252aeab90628
Taras Mikulskyi <tarasmikulskyi@ukr.net>
2023-11-08 21:05:22 +0200

    Created database config

---
+++ 09_auth_api/src/config/database.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+
+config();
+
+export const databaseConfig = {
+    host: process.env.DB_HOST || "localhost",
+    port: +(process.env.DB_PORT || "5432"),
+    database: process.env.DB_DATABASE || "postgres",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+};
