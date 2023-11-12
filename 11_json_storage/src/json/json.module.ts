0287c8f9d3fbd3bc9132dc7abd11970f526ac6a9
Taras Mikulskyi <tarasmikulskyi@ukr.net>
2023-11-12 13:28:40 +0200

    Created json module

---
+++ 11_json_storage/src/json/json.module.ts
@@ -0,0 +1,21 @@
+import { Application, Request, Response } from "express";
+import fs from "fs";
+import { JsonBaseController } from "./json.controller";
+
+
+export class JsonBaseModule {
+    jsonBaseController: JsonBaseController = new JsonBaseController();
+
+    constructor(private app: Application) {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.app.put("/:json_path", (req: Request, res: Response) =>
+            this.jsonBaseController.putJson(req, res)
+        );
+        this.app.get("/:json_path", (req: Request, res: Response) =>
+            this.jsonBaseController.getJson(req, res)
+        );
+    }
+}
